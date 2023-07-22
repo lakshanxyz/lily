efc8d23216a2a1770f56adec7c03a64e6669008f
Lakshan...! <108253179+lakshanxyz@users.noreply.github.com>
2023-07-22 20:36:54 +0530

    fixed with crap

--- plugins/download/mediafire.js
+++ plugins/download/mediafire.js
@@ -1,4 +1,5 @@
 const { decode } = require('html-entities')
+const { mediafire2 } = require('./../../scrape/mediafire')
 exports.run = {
    usage: ['mediafire'],
    hidden: ['mf'],
@@ -14,25 +15,27 @@ exports.run = {
          if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'https://www.mediafire.com/file/1fqjqg7e8e2v3ao/YOWA.v8.87_By.SamMods.apk/file'), m)
          if (!args[0].match(/(https:\/\/www.mediafire.com\/)/gi)) return client.reply(m.chat, global.status.invalid, m)
          client.sendReact(m.chat, '🕒', m.key)
-         let json = await Api.mediafire(args[0])
-         if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
+         let old = new Date()
+         let json = await mediafire2(args[0])
+       //if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
          let text = `乂  *M E D I A F I R E*\n\n`
-         text += '	◦  *Name* : ' + unescape(decode(json.data.filename)) + '\n'
-         text += '	◦  *Size* : ' + json.data.size + '\n'
-         text += '	◦  *Extension* : ' + json.data.extension + '\n'
-         text += '	◦  *Mime* : ' + json.data.mime + '\n'
-         text += '	◦  *Uploaded* : ' + json.data.uploaded + '\n\n'
+         text += '	◦  *Name* : ' + unescape(decode(json.filename)) + '\n'
+         text += '	◦  *Size* : ' + json.size + '\n'
+         text += '	◦  *Title* : ' + json.title + '\n'
+         text += '	◦  *Published* : ' + json.published + '\n'
+         text += '	◦  *Mime* : ' + json.mime + '\n'
+         text += `	◦  *Fetching* : ${((new Date - old) * 1)} ms\n\n`
          text += global.footer
-         let chSize = Func.sizeLimit(json.data.size, global.max_upload)
-         if (chSize.oversize) return client.reply(m.chat, `💀 File size (${json.data.size}) exceeds the maximum limit, download it by yourself via this link : ${await (await scrap.shorten(json.data.link)).data.url}`, m)
+         let chSize = Func.sizeLimit(json.size, global.max_upload)
+         if (chSize.oversize) return client.reply(m.chat, `❌ File size (${json.size}) exceeds the maximum limit, download it by yourself via this link : ${await (await Api.tinyurl(json.url)).data}`, m)
          client.sendMessageModify(m.chat, text, m, {
             largeThumb: true,
-            thumbnail: await Func.fetchBuffer('https://telegra.ph/file/fcf56d646aa059af84126.jpg')
+            thumbnail: await Func.fetchBuffer('https://telegra.ph/file/b2d5a31422827b0297871.jpg')
          }).then(async () => {
-            client.sendFile(m.chat, json.data.link, unescape(decode(json.data.filename)), '', m)
+            client.sendFile(m.chat, json.url, unescape(decode(json.filename)), '', m)
          })
-      } catch {
-         return client.reply(m.chat, global.status.error, m)
+      } catch (e) {
+         return client.reply(m.chat, Func.jsonFormat(e), m)
       }
    },
    error: false,
