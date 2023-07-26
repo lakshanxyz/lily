41dc6148270b63c18729f604f0c5cebb5bc4bba6
Lakshan...! <108253179+lakshanxyz@users.noreply.github.com>
2023-07-27 00:21:11 +0530

    thanks for hisoka

--- plugins/download/play.js
+++ plugins/download/play.js
@@ -1,4 +1,5 @@
-const { search } = require('./../../scrape/play');
+const { getInfo, chooseFormat } = require('ytdl-core')
+const { search } = require('yt-search')
 exports.run = {
    usage: ['play'],
    hidden: ['lagu', 'song', 'music'],
@@ -13,26 +14,23 @@ exports.run = {
       try {
          if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'lathi'), m)
          client.sendReact(m.chat, '🕒', m.key)
-         const srch = await search(text)
-         //if (!srch || srch.length == 0) return client.reply(m.chat, global.status.fail, m)
-         const json = await Func.fetchJson('https://yt.nxr.my.id/yt2?url=https://youtu.be/' + srch[0].videoId + '&type=audio')
-         if (!json.status || !json.data.url) return client.reply(m.chat, global.status.fail, m)
-         let caption = `乂  *Y T - P L A Y*\n\n`
-         caption += `	◦  *Title* : ${json.title}\n`
-         caption += `	◦  *Size* : ${json.data.size}\n`
-         caption += `	◦  *Duration* : ${json.duration}\n`
-         caption += `	◦  *Bitrate* : ${json.data.quality}\n\n`
-         caption += global.footer
-         let chSize = Func.sizeLimit(json.data.size, global.max_upload)
-         if (chSize.oversize) return client.reply(m.chat, ` File size (${json.data.size}) is too large, download it by yourself via this link : ${await (await Api.tinyurl(json.data.url)).data}`, m)
+         const srch = await search(m.text).then((res) => res.videos[0]);
+         let yt = await getInfo(srch.url)
+         let { url, quality, contentLength } = chooseFormat(yt.formats, { quality: "highestaudio", filter: "audioonly" })
+         let caption = `
+${yt.videoDetails.title} 
+
+${yt.videoDetails.author.name} (${yt.videoDetails.author.user_url})
+
+${yt.videoDetails.description}
+`;
          client.sendMessageModify(m.chat, caption, m, {
             largeThumb: true,
-            thumbnail: await Func.fetchBuffer(json.thumbnail),
-            url: global.db.setting.link
+            thumbnail: await Func.fetchBuffer(yt.videoDetails.thumbnails.slice(-1)[0].url)
          }).then(async () => {
-            client.sendFile(m.chat, json.data.url, json.data.filename, '', m, {
+            client.sendFile(m.chat, url, yt.videoDetails.title, '', m, {
                document: false,
-               APIC: await Func.fetchBuffer(json.thumbnail)
+               APIC: await Func.fetchBuffer(yt.videoDetails.thumbnails.slice(-1)[0].url)
             })
          })
       } catch (e) {
